1383682f924d15d96209b0e31a16be0da8ee3302
YU JEONG SEON <yjs000616@gmail.com>
2023-03-08 01:22:34 +0900

    [FEAT] calendar 넣기

--- app/src/main/java/com/example/free_pre_android/SymptomActivity.kt
+++ app/src/main/java/com/example/free_pre_android/SymptomActivity.kt
@@ -1,5 +1,6 @@
 package com.example.free_pre_android
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.free_pre_android.databinding.ActivitySymptomBinding
@@ -10,5 +11,62 @@ class SymptomActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         viewBinding = ActivitySymptomBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
+
+        viewBindingRun()
+    }
+
+    fun viewBindingRun(){
+        viewBinding.run {
+            textHeadache.setOnClickListener {
+                textHeadache.setBackgroundResource(R.color.primary_light)
+                textHeadache.setTextColor(Color.parseColor("#1A2A46"))
+            }
+            textBreastPain.setOnClickListener {
+                textBreastPain.setBackgroundResource(R.color.primary_light)
+                textBreastPain.setTextColor(Color.parseColor("#1A2A46"))
+            }
+            textDepressed.setOnClickListener {
+                textDepressed.setBackgroundResource(R.color.primary_light)
+                textDepressed.setTextColor(Color.parseColor("#1A2A46"))
+            }
+            textFatigue.setOnClickListener {
+                textFatigue.setBackgroundResource(R.color.primary_light)
+                textFatigue.setTextColor(Color.parseColor("#1A2A46"))
+            }
+            textAnxious.setOnClickListener {
+                textAnxious.setBackgroundResource(R.color.primary_light)
+                textAnxious.setTextColor(Color.parseColor("#1A2A46"))
+            }
+            textSwollenBreast.setOnClickListener {
+                textSwollenBreast.setBackgroundResource(R.color.primary_light)
+                textSwollenBreast.setTextColor(Color.parseColor("#1A2A46"))
+            }
+            textAppetiteChange.setOnClickListener {
+                textAppetiteChange.setBackgroundResource(R.color.primary_light)
+                textAppetiteChange.setTextColor(Color.parseColor("#1A2A46"))
+            }
+            textConstipation.setOnClickListener {
+                textConstipation.setBackgroundResource(R.color.primary_light)
+                textConstipation.setTextColor(Color.parseColor("#1A2A46"))
+            }
+            textDiarrhea.setOnClickListener {
+                textDiarrhea.setBackgroundResource(R.color.primary_light)
+                textDiarrhea.setTextColor(Color.parseColor("#1A2A46"))
+            }
+            textDema.setOnClickListener {
+                textDema.setBackgroundResource(R.color.primary_light)
+                textDema.setTextColor(Color.parseColor("#1A2A46"))
+            }
+            textBackPain.setOnClickListener {
+                textBackPain.setBackgroundResource(R.color.primary_light)
+                textBackPain.setTextColor(Color.parseColor("#1A2A46"))
+            }
+
+
+
+
+
+
+        }
     }
 }
