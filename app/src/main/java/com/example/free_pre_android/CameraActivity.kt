ff3dfb74e4ef6e466f2b1fda21fe8ba1ed631835
flora7883 <flora7883@gmail.com>
2023-04-22 05:00:16 +0900

    [MOD] google cloud key 삭제

--- app/src/main/java/com/example/free_pre_android/CameraActivity.kt
+++ app/src/main/java/com/example/free_pre_android/CameraActivity.kt
@@ -24,11 +24,6 @@ class CameraActivity : BaseActivity() {
     //private var mediaPlayer: MediaPlayer? = null
     private lateinit var viewBinding: ActivityCameraBinding
 
-    companion object{
-        private val CLOUD_VISION_API_KEY: String = "AIzaSyBHdJZ1o4uGmQ5PE6j9zUGIGXtJIYy7650"
-
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewBinding = ActivityCameraBinding.inflate(layoutInflater)
